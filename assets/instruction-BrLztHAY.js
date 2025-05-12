eeca674a8a3279c9434ccd384a121826029d4f94
ayugunin <ayugunin@sberautotech.ru>
2025-05-12 21:24:44 +0300

    Updates

--- assets/instruction-BrLztHAY.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-DPRp_zFp.js";function r(){return t.jsx("div",{children:"Instruction"})}export{r as default};
---
+++ assets/instruction-Dozitt6v.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-BX7Ni4qn.js";function r(){return t.jsx("div",{children:"Instruction"})}export{r as default};
