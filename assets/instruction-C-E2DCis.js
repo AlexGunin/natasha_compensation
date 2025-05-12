200bc69332e9bc54d3c9e8d700b04f63f1fb1746
ayugunin <ayugunin@sberautotech.ru>
2025-05-12 21:34:43 +0300

    Updates

---
+++ assets/instruction-C-E2DCis.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-BJ6SHk6A.js";function r(){return t.jsx("div",{children:"Instruction"})}export{r as default};
--- assets/instruction-Dozitt6v.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-BX7Ni4qn.js";function r(){return t.jsx("div",{children:"Instruction"})}export{r as default};
